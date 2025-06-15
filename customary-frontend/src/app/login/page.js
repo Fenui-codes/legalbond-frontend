9e988b1e7cfc6114439a1482eb0c528595294437
Fenui-codes <146883176+Fenui-codes@users.noreply.github.com>
2025-06-15 22:15:16 +0100

    Update page.js

--- customary-frontend/src/app/login/page.js
+++ customary-frontend/src/app/login/page.js
@@ -1,4 +1,4 @@
-'use client';
+/*'use client';
 
 import { useState } from 'react';
 import Navbar from '../components/Navbar';
@@ -90,4 +90,4 @@ export default function Login() {
       </section>
     </main>
   );
-}
+}*/
